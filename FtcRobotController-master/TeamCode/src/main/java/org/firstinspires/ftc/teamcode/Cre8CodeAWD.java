2d00fa5183d84e15cc2ddc051baef706f7b97165
Space <89947572+SpaceFrost@users.noreply.github.com>
2023-12-14 21:09:37 -0600

    Add files via upload

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cre8CodeAWD.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cre8CodeAWD.java
@@ -31,6 +31,7 @@ public class Cre8CodeAWD extends LinearOpMode
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        riggingActivate =  hardwareMap.get(DcMotor.class, "activate");
         bayServo = hardwareMap.get(Servo.class, "bay");
         droneLauncher = hardwareMap.get(Servo.class, "launcher");
         //variables
@@ -43,7 +44,8 @@ public class Cre8CodeAWD extends LinearOpMode
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.FORWARD);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        riggingActivate.setDirection(DcMotorSimple.Direction.FORWARD);
+        riggingDeploy.setDirection(DcMotorSimple.Direction.FORWARD);
         waitForStart();
 
         if (opModeIsActive()) {
@@ -57,7 +59,7 @@ public class Cre8CodeAWD extends LinearOpMode
 
 
                 /***********************************************
-                 * MAIN DRIVER CONTROLS   *** GAMEPAD1
+                 * Movement DRIVER CONTROLS   *** GAMEPAD1
                  * *********************************************
                  */
                 backRight.setPower(-gamepad1.left_stick_y);
@@ -76,9 +78,14 @@ public class Cre8CodeAWD extends LinearOpMode
                 telemetry.addData("Left Pow", frontLeft.getPower());
                 telemetry.addData("Right Pow", frontLeft.getPower());
                 telemetry.update();
-
+				double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
+                double rx = gamepad1.right_stick_x;
+                backLeft.setPower(y + rx);
+                backRight.setPower(y - rx);
+                frontLeft.setPower(y + rx);
+                frontRight.setPower(y - rx);
                 /******************************************************
-                 * Secondary Driver ***** GAMEPAD2
+                 * Arm Driver ***** GAMEPAD2
                  * ****************************************************
                  */
                 //Bay Closed
@@ -99,64 +106,17 @@ public class Cre8CodeAWD extends LinearOpMode
                 if(gamepad2.dpad_down){
                     droneLauncher.setPosition(close);
                 }
-                //rigging activate
-                if(gamepad2.right_bumper){
-                    activateRigging(1,2000);
-                }
-                //rigging deploy
-                if(gamepad2.left_bumper){
-                    deployRigging(1,2000);
-                }
-                /*if(gamepad2.y) {
-                    // move to 0 degrees.
-                    arm.setPosition(0);
-                } else if (gamepad2.x || gamepad2.b) {
-                    // move to 90 degrees.
-                    arm.setPosition(0.5);
-                } else if (gamepad2.a) {
-                    // move to 180 degrees.
-                    arm.setPosition(1);
-                }
-                telemetry.addData("Servo Position", arm.getPosition());
-                telemetry.update();*/
-
-                double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
-                double rx = gamepad1.right_stick_x;
-                backLeft.setPower(y + rx);
-                backRight.setPower(y - rx);
-                frontLeft.setPower(y + rx);
-                frontRight.setPower(y - rx);
-                /*double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
-                double x = gamepad1.left_stick_x;
-                double rx = gamepad1.right_stick_x;
-                frontLeft.setPower(y + x + rx);
-                backLeft.setPower(y - x + rx);
-                frontRight.setPower(y - x - rx);
-                backRight.setPower(y + x - rx);*/
+				//rigging
+                riggingActivate.setPower(-gamepad2.left_stick_y);
+                telemetry.addData("Left Pow", riggingActivate.getPower());
+                telemetry.addData("Right Pow", riggingActivate.getPower());
+                telemetry.update();
+                riggingDeploy.setPower(-gamepad2.right_stick_y);
+                telemetry.addData("Left Pow", riggingDeploy.getPower());
+                telemetry.addData("Right Pow", riggingDeploy.getPower());
+                telemetry.update();
             }
         }
-    }
-    public void rigging1(double power)
-    {
-        riggingActivate.setDirection(DcMotorSimple.Direction.FORWARD);
-        riggingActivate.setPower(power);
-    }
-
-    public void activateRigging (double power, long time) throws InterruptedException
-    {
-        rigging1(power);
-        Thread.sleep(time);
-    }
-    public void rigging2(double power)
-    {
-        riggingActivate.setDirection(DcMotorSimple.Direction.FORWARD);
-        riggingActivate.setPower(power);
-    }
-
-    public void deployRigging (double power, long time) throws InterruptedException
-    {
-        rigging2(power);
-        Thread.sleep(time);
     }
 }
 
