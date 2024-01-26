4eb92a51ee6691278a0c39d09c3b2044dc5af299
Space <89947572+SpaceFrost@users.noreply.github.com>
2024-01-25 18:53:33 -0600

    awdon

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cre8CodeAWD.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cre8CodeAWD.java
@@ -6,21 +6,23 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-@TeleOp(name = "Cre8CodeAWD")
+@TeleOp(name = "awdon")
 public class Cre8CodeAWD extends LinearOpMode
 {
 
     private DcMotor frontRight;
-    private DcMotor frontLeft;
+    private DcMotor animal;
     private DcMotor backRight;
     private DcMotor backLeft;
-    private DcMotor riggingActivate;
-    private DcMotor riggingDeploy;
-    private DcMotor intake;
-    private DcMotor backdrop;
-    private Servo droneLauncher;
-    private Servo enkulu;
-    private Servo amu;
+    /*private DcMotor parjanya;
+    private DcMotor intakke;
+    private DcMotor zewp;
+    private DcMotor zow;
+    private Servo ninjalauncher;
+    //private Servo enkulu;
+    //private Servo amu;
+    private Servo boxy;*/
+    
 
 
 
@@ -31,28 +33,29 @@ public class Cre8CodeAWD extends LinearOpMode
     public void runOpMode() throws InterruptedException {
         //mapping motors and servos
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        frontLeft = hardwareMap.get(DcMotor.class, "animal");
+        animal = hardwareMap.get(DcMotor.class, "animal");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        riggingActivate =  hardwareMap.get(DcMotor.class, "activate");
-        riggingDeploy =  hardwareMap.get(DcMotor.class, "parjanya");
-        intake = hardwareMap.get(DcMotor.class, "intake");
-        backdrop = hardwareMap.get(DcMotor.class, "backdrop");
-        droneLauncher = hardwareMap.get(Servo.class, "launcher");
-        enkulu = hardwareMap.get(Servo.class, "enkulu");
-        amu = hardwareMap.get(Servo.class, "amu");
+        /*parjanya =  hardwareMap.get(DcMotor.class, "parjanya");
+        intakke = hardwareMap.get(DcMotor.class, "intakke");
+        zewp = hardwareMap.get(DcMotor.class, "zewp");
+        zow = hardwareMap.get(DcMotor.class, "zow");
+        ninjalauncher = hardwareMap.get(Servo.class, "ninja");
+        //enkulu = hardwareMap.get(Servo.class, "enkulu");
+        //amu = hardwareMap.get(Servo.class, "amu");
+        boxy = hardwareMap.get(Servo.class, "boxyboxbox");*/
         //variables
-        double bayOpen = 0.5;
-        double close = 0.0;
-        double launcherOpen = 1.0;
 
         //Motor directions
         frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        animal.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        riggingActivate.setDirection(DcMotorSimple.Direction.FORWARD);
-        riggingDeploy.setDirection(DcMotorSimple.Direction.FORWARD);
+        /*zewp.setDirection(DcMotorSimple.Direction.FORWARD);
+        zow.setDirection(DcMotorSimple.Direction.FORWARD);
+        parjanya.setDirection(DcMotorSimple.Direction.FORWARD);
+        intakke.setDirection(DcMotorSimple.Direction.FORWARD);*/
+
         waitForStart();
 
         if (opModeIsActive()) {
@@ -81,58 +84,68 @@ public class Cre8CodeAWD extends LinearOpMode
                 telemetry.addData("Left Pow", frontRight.getPower());
                 telemetry.addData("Right Pow", frontRight.getPower());
                 telemetry.update();
-                frontLeft.setPower(-gamepad1.left_stick_y);
-                telemetry.addData("Left Pow", frontLeft.getPower());
-                telemetry.addData("Right Pow", frontLeft.getPower());
+                animal.setPower(-gamepad1.left_stick_y);
+                telemetry.addData("Left Pow", animal.getPower());
+                telemetry.addData("Right Pow", animal.getPower());
+                telemetry.update();
+                //Turns
+                /*double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
+                double rx = gamepad1.right_stick_x;
+                backLeft.setPower(y - rx);
+                backRight.setPower(y + rx);
+                animal.setPower(y - rx);
+                frontRight.setPower(y + rx);
+                //intakke
+                intakke.setPower(-gamepad1.right_trigger);
+                telemetry.addData("Left1 Pow", intakke.getPower());
+                telemetry.addData("Right Pow", intakke.getPower());
                 telemetry.update();
 
-                /******************************************************
+                *//******************************************************
                  * Arm Driver ***** GAMEPAD2
                  * ****************************************************
-                 */
+                 *//*
                 //drone
                 if(gamepad2.a){
-                    droneLauncher.setPosition(0.5);
+                    ninjalauncher.setPosition(90);
                 }
                 if(gamepad2.b){
-                    droneLauncher.setPosition(0);
+                    ninjalauncher.setPosition(0);
                 }
                 //enkulu amus powerlifter
-                if(gamepad2.left_bumper){
+                *//*if(gamepad2.left_bumper){
                     enkulu.setPosition(0.5);
                     amu.setPosition(0.5);
                 }
                 if(gamepad2.dpad_down){
                     enkulu.setPosition(0);
                     amu.setPosition(0);
+                }*//*
+                //box
+                if(gamepad2.x){
+                    boxy.setPosition(0);
                 }
-                //Intake
-                intake.setPower(-gamepad2.right_trigger);
-                telemetry.addData("Left1 Pow", riggingActivate.getPower());
-                telemetry.addData("Right Pow", riggingActivate.getPower());
-                telemetry.update();
+                if(gamepad2.y){
+                    boxy.setPosition(70);
+                }
                 //backdrop
-                backdrop.setPower(-gamepad2.right_stick_y);
-                telemetry.addData("Left1 Pow", riggingActivate.getPower());
-                telemetry.addData("Right Pow", riggingActivate.getPower());
+                zewp.setPower(-gamepad2.right_stick_y);
+                telemetry.addData("Left Pow", zewp.getPower());
+                telemetry.addData("Right Pow", zewp.getPower());
+                telemetry.update();
+                zow.setPower(-gamepad2.left_stick_y);
+                telemetry.addData("Left Pow", zow.getPower());
+                telemetry.addData("Right Pow", zow.getPower());
                 telemetry.update();
                 //rigging
-                riggingActivate.setPower(-gamepad2.left_trigger);
-                telemetry.addData("Left1 Pow", riggingActivate.getPower());
-                telemetry.addData("Right Pow", riggingActivate.getPower());
+                parjanya.setPower(-gamepad2.left_trigger);
+                telemetry.addData("Left Pow", parjanya.getPower());
+                telemetry.addData("Right Pow", parjanya.getPower());
                 telemetry.update();
-                riggingDeploy.setPower(-gamepad2.left_stick_y);
-                telemetry.addData("Left Pow", riggingDeploy.getPower());
-                telemetry.addData("Right Pow", riggingDeploy.getPower());
-                telemetry.update();
-
-                //Turns
-                double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
-                double rx = gamepad1.right_stick_x;
-                backLeft.setPower(y - rx);
-                backRight.setPower(y + rx);
-                frontLeft.setPower(y - rx);
-                frontRight.setPower(y + rx);
+                parjanya.setPower(gamepad2.right_trigger);
+                telemetry.addData("Left Pow", parjanya.getPower());
+                telemetry.addData("Right Pow", parjanya.getPower());
+                telemetry.update();*/
             }
         }
     }
